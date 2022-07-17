87181524b3642ba16e3a26e1ad27c53d55c60977
shuaixr <1025sxr@gmail.com>
2022-07-18 01:12:00 +0800

    Init

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Button from '@smui/button';
+</script>
+
+<Button />
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
